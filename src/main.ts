13301596bf77acc39a31f9bfd119f674311da62b
blocks1245 <meneersander@gmail.com>
2025-09-14 00:08:32 +0200

    maybe this will work?

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,29 @@
-
 import { NestFactory } from '@nestjs/core';
-import serverlessExpress from '@codegenie/serverless-express';
+import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
+
 import { AppModule } from './app.module';
 
 let server: Handler;
 
-async function bootstrap(): Promise<Handler> {
-  const app = await NestFactory.create(AppModule);
-  await app.init();
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.init();
 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+    const expressApp = app.getHttpAdapter().getInstance();
+    return serverlessExpress({ app: expressApp });
 }
 
+// Initialize the server once and cache it
+const initializeServer = bootstrap().then((initializedServer) => {
+    server = initializedServer;
+});
+
 export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
+    event: any,
+    context: Context,
+    callback: Callback,
 ) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
+    await initializeServer;
+    return server(event, context, callback);
 };

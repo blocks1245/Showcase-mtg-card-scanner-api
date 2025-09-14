96ba7a1c4f655c61bc4d5374230876f10522913b
blocks1245 <meneersander@gmail.com>
2025-09-14 12:39:30 +0200

    ec2?

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import serverlessExpress from '@vendia/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
-
 import { AppModule } from './app.module';
 
-let server: Handler;
-
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.init();
-
-    const expressApp = app.getHttpAdapter().getInstance();
-    return serverlessExpress({ app: expressApp });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-// Initialize the server once and cache it
-const initializeServer = bootstrap().then((initializedServer) => {
-    server = initializedServer;
-});
-
-export const handler: Handler = async (
-    event: any,
-    context: Context,
-    callback: Callback,
-) => {
-    await initializeServer;
-    return server(event, context, callback);
-};
+bootstrap();
